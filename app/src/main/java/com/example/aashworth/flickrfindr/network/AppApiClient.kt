e728796e96ac8203f2e9ccd5acbd7487d52e4704
Alex <alex.ashworth@turner.com>
2019-03-31 12:56:22 -0400

    implement AppApiClient
    - add implementation for searchPhotos()

--- app/src/main/java/com/example/aashworth/flickrfindr/network/AppApiClient.kt
+++ app/src/main/java/com/example/aashworth/flickrfindr/network/AppApiClient.kt
@@ -1,5 +1,13 @@
 package com.example.aashworth.flickrfindr.network
 
+import com.example.aashworth.flickrfindr.InjectorUtils
+import com.example.aashworth.flickrfindr.data.models.Photo
+import retrofit2.Call
+import javax.inject.Inject
+
 object  AppApiClient {
-    
+    fun searchPhotos(searchTerm: String ) : Call<List<Photo>> {
+        val flickrPhotoService = InjectorUtils.getFlickrPhotoService()
+        return  flickrPhotoService.searchPhotos(searchTerm)
+    }
 }
