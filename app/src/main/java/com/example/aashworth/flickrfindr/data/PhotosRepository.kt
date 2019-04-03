4254524f00485a9f2dfe4d05c421f3504a2e1d98
Alex <alex.ashworth@turner.com>
2019-04-03 16:28:34 -0400

    add error handling for repository class

--- app/src/main/java/com/example/aashworth/flickrfindr/data/PhotosRepository.kt
+++ app/src/main/java/com/example/aashworth/flickrfindr/data/PhotosRepository.kt
@@ -1,19 +1,25 @@
 package com.example.aashworth.flickrfindr.data
 
 import android.arch.lifecycle.MutableLiveData
+import android.util.Log
 import com.example.aashworth.flickrfindr.data.models.Photo
 import com.example.aashworth.flickrfindr.network.AppApiClient
 import kotlinx.coroutines.*
 
 class PhotosRepository(private val appApiClient: AppApiClient) {
+    val TAG = "PhotosRepository";
 
     fun getPhotosForSearchTerm(searchTerm: String): MutableLiveData<List<Photo>> {
         var photosLiveData = MutableLiveData<List<Photo>>()
 
         CoroutineScope(Dispatchers.IO).launch {
-            val result = appApiClient.searchPhotos(searchTerm).await()
-            withContext(Dispatchers.Main) {
-                photosLiveData.value = result.photos.photosList
+            try {
+                val result = appApiClient.searchPhotos(searchTerm).await()
+                withContext(Dispatchers.Main) {
+                    photosLiveData.value = result.photos.photosList
+                }
+            }catch (e: Throwable) {
+                Log.e(TAG, "Error executing search request")
             }
         }
         return photosLiveData
